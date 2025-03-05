5a63d7fd1af6b66ead05c3a0316b415a59f2f052
Mohinur0321 <mokhinurernazarova890@gmail.com>
2025-03-05 22:19:17 +0500

    Update lesson2.sql

--- lesson-2/homework/lesson2.sql
+++ lesson-2/homework/lesson2.sql
@@ -1,1 +1,22 @@
+CREATE DATABASE company_db
+CREATE TABLE Employees(
+EmpID INT PRIMARY KEY NOT NULL, -- id required
+[NAME] VARCHAR(50) NOT NULL, -- name required
+SALARY DECIMAL (10,2) NOT NULL -- salary required
+)
+INSERT INTO Employees (EmpID, [NAME], SALARY)
+VALUES
+(1, 'John', 1000),
+(2, 'Doe', 2000),
+(3, 'Maria', 3000);
 
+UPDATE Employees
+SET SALARY = 5
+WHERE EmpID = 1;
+
+DELETE FROM Employees WHERE EmpID = 2;
+ALTER TABLE Employees
+ALTER COLUMN [Name] VARCHAR(100)
+
+--ALTER TABLE Employees
+--ADD COLUMN Department Varchar(50);
